bec05f867e0c654e94cebfea1782d76d23f5bfeb
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-01-10 21:03:24 +0200

    Solved problem

--- src/StacksAndQueues/SimpleCalculator_02.java
+++ src/StacksAndQueues/SimpleCalculator_02.java
@@ -1,4 +1,30 @@
 package StacksAndQueues;
 
+import java.util.*;
+
 public class SimpleCalculator_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] tokens = input.split("\\s+");
+        List<String> parts = new ArrayList<>(List.of(tokens));
+        Collections.reverse(parts);
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        for(String part:parts){
+            stack.push(part);
+        }
+        while (stack.size()>1){
+            int first = Integer.parseInt(stack.pop());
+            String op = stack.pop();
+            int second = Integer.parseInt(stack.pop());
+            int result = 0;
+            switch (op){
+                case"+": result=first+second; break;
+                case"-": result=first-second; break;
+            }
+            stack.push(""+result);
+        }
+        System.out.println(stack.peek());
+    }
 }
