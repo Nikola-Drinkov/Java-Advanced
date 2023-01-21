4c767a882702d44463f535943931bc79a63ed045
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-01-21 02:02:50 +0200

    Solved problem

--- src/StreamsFilesDirectories/ExtractIntegers_04.java
+++ src/StreamsFilesDirectories/ExtractIntegers_04.java
@@ -3,16 +3,19 @@ package StreamsFilesDirectories;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class ExtractIntegers_04 {
     public static void main(String[] args) throws FileNotFoundException {
 
         String path = "D:\\Java Projects\\Java Advanced\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\input.txt";
+        String pathOut = "D:\\Java Projects\\Java Advanced\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\04output.txt";
         Scanner sc = new Scanner(new FileInputStream(path));
+        PrintStream writer = new PrintStream(pathOut);
         while (sc.hasNext()){
             if(sc.hasNextInt()){
-                System.out.println(sc.nextInt());
+                writer.println(sc.nextInt());
             }
             sc.next();
         }
