6743e9fb501013ca31d38a9705ba1a91f7a5e51d
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-01-22 20:54:09 +0200

    Solved problem

--- src/MultidimensionalArrs_Exercise/MaximalSum_04.java
+++ src/MultidimensionalArrs_Exercise/MaximalSum_04.java
@@ -10,6 +10,26 @@ public class MaximalSum_04 {
 
         int[][] matrix = new int [rows][cols];
         fillMatrix(matrix,rows,cols,sc);
+
+        int maxSum=Integer.MIN_VALUE;
+        int topR = 0;
+        int topC = 0;
+        for(int row=0; row<rows-2; row++) {
+            for (int col=0; col<cols-2; col++) {
+                int currentSum =    matrix[row][col] + matrix[row][col + 1] + matrix[row][col + 2] +
+                                    matrix[row + 1][col] + matrix[row + 1][col + 1] + matrix[row + 1][col + 2] +
+                                    matrix[row + 2][col] + matrix[row + 2][col + 1] + matrix[row + 2][col + 2];
+                if (currentSum>maxSum) {
+                    maxSum = currentSum;
+                    topR=row;
+                    topC=col;
+                }
+            }
+        }
+        System.out.println("Sum = "+maxSum);
+        System.out.println(matrix[topR][topC]+" "+matrix[topR][topC+1]+" "+matrix[topR][topC+2]);
+        System.out.println(matrix[topR+1][topC]+" "+matrix[topR+1][topC+1]+" "+matrix[topR+1][topC+2]);
+        System.out.println(matrix[topR+2][topC]+" "+matrix[topR+2][topC+1]+" "+matrix[topR+2][topC+2]);
     }
     public static void fillMatrix(int[][] matrix,int rows, int cols, Scanner sc){
         for(int row=0; row<rows; row++){
