afe5777b4b87ec00fbc6b824fd002daabfef2e42
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-01-23 20:07:09 +0200

    Solved problem

--- src/StreamsFilesDirectories_Exercise/AllCapitals_03.java
+++ src/StreamsFilesDirectories_Exercise/AllCapitals_03.java
@@ -1,5 +1,8 @@
 package StreamsFilesDirectories_Exercise;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,9 +11,13 @@ import java.util.List;
 public class AllCapitals_03 {
     public static void main(String[] args) throws IOException {
         String path = "D:\\Java Projects\\Java Advanced\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\input.txt";
+        File file = new File("D:\\Java Projects\\Java Advanced\\04. Java-Advanced-Files-and-Streams-Exercises-Resources\\03output.txt");
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
         List<String> lines = Files.readAllLines(Path.of(path));
         for(String line:lines){
-            System.out.println(line.toUpperCase());
+            writer.write(line.toUpperCase());
+            writer.newLine();
         }
+        writer.close();
     }
 }
