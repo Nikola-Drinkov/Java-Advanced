2e10b94839ddaf95198964709db764985158aca6
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-02-08 20:30:47 +0200

    Solved problem

--- IteratorsComparators/src/Book/Main.java
+++ IteratorsComparators/src/Book/Main.java
@@ -1,19 +1,15 @@
 package Book;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
         Book bookThree = new Book("The Documents in the Case", 2002);
         Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
 
-        List<Book> books = new ArrayList<>();
-        books.add(bookOne);
-        books.add(bookTwo);
-        books.add(bookThree);
+        Library<Book> library = new Library<Book>(bookOne, bookTwo, bookThree);
 
+        for (Book book : library) {
+            System.out.println(book.getTitle());
+        }
     }
 }
